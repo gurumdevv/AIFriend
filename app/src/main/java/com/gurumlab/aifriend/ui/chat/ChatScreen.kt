9a57fb7a2079fb30a2f64c618e513265d2cd65fc
gurumdevv <gurumdevv@gmail.com>
2024-11-29 00:42:26 +0900

    feat: LazyColumn 아래에 padding을 추가해서 채팅 갯수가 채팅 창을 채울만큼 충분하지 않은 경우에도 채팅이 상단부터 보이도록 구현
    발견된 문제: 키보드가 올라올 때 스크롤이 바운스 하는 문제가 있음

--- app/src/main/java/com/gurumlab/aifriend/ui/chat/ChatScreen.kt
+++ app/src/main/java/com/gurumlab/aifriend/ui/chat/ChatScreen.kt
@@ -1,8 +1,10 @@
 package com.gurumlab.aifriend.ui.chat
 
 import android.content.Context
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Ease
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.background
@@ -11,6 +13,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -19,15 +22,12 @@ import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -47,6 +47,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -56,6 +57,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
@@ -105,7 +107,7 @@ fun ChatContent(
     var inputFieldHeight by remember { mutableIntStateOf(0) }
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
-    val scrollState = rememberLazyListState()
+    val listState = rememberLazyListState()
 
 //    val jumpToBottomButtonEnabled by remember {
 //        derivedStateOf {
@@ -141,7 +143,7 @@ fun ChatContent(
         ) {
             Messages(
                 messages = chatMessages,
-                scrollState = scrollState,
+                listState = listState,
                 context = context,
                 modifier = Modifier.weight(1f)
             )
@@ -183,10 +185,35 @@ fun ChatContent(
 @Composable
 fun Messages(
     messages: LazyPagingItems<ChatMessage>,
-    scrollState: LazyListState,
+    listState: LazyListState,
     context: Context,
     modifier: Modifier = Modifier
 ) {
+    var lazyColumHeightPx by remember { mutableFloatStateOf(0f) }
+    val layoutInfo = remember { derivedStateOf { listState.layoutInfo } }
+    val totalItemHeight = layoutInfo.value.visibleItemsInfo.sumOf { it.size }
+
+    //TODO: 키보드가 올라온 경우 대응 필요
+//    LaunchedEffect(lazyColumHeightPx) {
+//        Log.d("ChatScreen", "lazyColumHeightPx: $lazyColumHeightPx")
+//    }
+//    LaunchedEffect(totalItemHeight) {
+//        Log.d("ChatScreen", "totalItemHeight: $totalItemHeight")
+//    }
+
+    val paddingBottomPx = if (totalItemHeight < lazyColumHeightPx) {
+        lazyColumHeightPx - totalItemHeight
+    } else {
+        0f
+    }
+    val paddingBottomDp = with(LocalDensity.current) { paddingBottomPx.toDp() }
+
+//    LaunchedEffect(paddingBottomDp) {
+//        Log.d("ChatScreen", "paddingBottomDp: $paddingBottomDp")
+//    }
+
+    val animatedPadding by animateDpAsState(targetValue = paddingBottomDp, label = "")
+
     var isShowList by remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
@@ -201,9 +228,14 @@ fun Messages(
             enter = fadeIn(animationSpec = tween(durationMillis = 100, easing = Ease))
         ) {
             LazyColumn(
-                state = scrollState,
+                state = listState,
                 reverseLayout = true,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
+                    .onGloballyPositioned {
+                        lazyColumHeightPx = it.size.height.toFloat()
+                    },
+                contentPadding = PaddingValues(bottom = animatedPadding)
             ) {
                 items(
                     count = messages.itemCount,
